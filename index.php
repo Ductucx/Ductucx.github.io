dff7200434ef12d1639cf254ede234336d29c1e2
Ductucx <dutuceveandres@gmail.com>
2026-08-01 21:41:40 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Ductucx</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300&display=swap" rel="stylesheet">
+        <script src="https://kit.fontawesome.com/b69ac6b51c.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="./CSS/estilos.css">
+    </head>
+    <body>
+        <!--Menú de navegación-->
+
+        <!--FIN Menú de navegación-->
+
+            <!--Bloque de inicio-->
+
+            <!--FIN Bloque de inicio-->
+
+            <!--Bloque sobre HTML-->
+
+            <!--FIN Bloque sobre HTML-->
+
+            <!--Bloque Portfolio-->
+
+            <!--FIN Bloque Portfolio-->
+
+            <!--Bloque video-->
+
+            <!--FIN Bloque video-->
+
+            <!--Bloque Experiencia-->
+
+            <!--FIN Bloque Experiencia-->
+
+            <!--Bloque contactar-->
+
+            <!--FIN Bloque contactar-->
+
+        <!--Footer o pie de página-->
+
+        <!--FIN Footer o pie de página-->
+
+    </body>
+</html>
